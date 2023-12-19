6e63db6d97bab7b5a797dc815e8f1561f0724b67
ishanlearnsprog <learnprog21@gmail.com>
2023-12-19 23:10:18 +0530

    Format Update

--- countdown.py
+++ countdown.py
@@ -7,6 +7,8 @@ while check != 0:
     end = datetime(curr.year, 11, 28)
     if end <= curr: end = datetime(curr.year + 1, 11, 28)
     
-    print("\n{}".format(end - curr))
+    days, timestamp = str((end - curr)).split(",")
+    hours, mins, _ = timestamp.split(":")
+    print("\n{}\n{} hours\n{} minutes".format(days, hours, mins))
 
     check = int(input("\nCheck Again ? 1 : 0 = "))
